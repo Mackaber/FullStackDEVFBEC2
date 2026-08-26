a6924a3ad4e3085b50ba85778ed36eb54a4ae4f3
Mackaber <mkbravo555@gmail.com>
2026-08-25 19:58:56 -0600

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const cors = require('cors')
+const pg = require('pg')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const PORT = 3000
+const server = express()
+
+// Conectar a la base de datos
+CONNECTION_STRING = process.env.PG_CONNECTION_STRING
+
+const client = new pg.Client(CONNECTION_STRING)
+
+
+server.use(cors())
+server.use(express.text())
+
+// BACKEND RECIBE EL INPUT DEL FRONTEND
+server.post("/test", async (request, response) => {
+
+    console.log("BODY EN EL BACKEND: ", request.body)
+
+    const name = request.body    
+    // BASE DE DATOS: INSERTAR EL INPUT EN LA TABLA USERS
+
+    // SIMULAR UNA INYECCIÓN SQL MALICIOSA
+    // const name = "'); DROP TABLE users; --"
+
+    client.query(`INSERT INTO users (name) values ($1)`, [name], (err, res) => {
+        if (err) {
+            console.error("DATABASE ERROR: ", err)
+        } else {
+            console.log("INSERTED", res)
+        }
+    })
+
+    // BASE DE DATOS: LEER TODOS LOS REGISTROS DE LA TABLA USERS
+    const results = await client.query("SELECT * FROM users", (err, res) => {
+        if (err) {
+            console.error("DATABASE ERROR: ", err)
+            response.status(500).send("Error en la base de datos")
+        } else {
+            console.log("ROWS: ", res.rows)
+            response.send(res.rows)
+        }
+    })
+})
+
+
+server.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`)
+
+    client.connect()
+    console.log("Conectado a la base de datos")
+})
---
+++ frontend/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [input, setInput] = useState("");
+
+  function handleInputChange(e) {
+    setInput(e.target.value)
+  }
+
+  async function handleBtnClick() {
+
+    console.log("BODY EN EL FRONTEND: ", input)
+
+    // FRONTEND ENVIA EL INPUT AL BACKEND
+    const response = await fetch("http://localhost:3000/test", 
+      { method: "POST", body: input }
+    )
+    console.log(response)
+    
+    const text = response.text()
+    console.log(text)
+  }
+
+  return (
+    <>
+      <input type="text" onChange={handleInputChange} value={input} />
+      <button onClick={handleBtnClick} >Enviar</button>
+    </>
+  )
+}
+
+export default App
